e4453b5364a3e01604d3d56808f7a53a3c0d8ba2
leejy12 <leejy31415@gmail.com>
2022-07-07 13:37:38 +0000

    new game setup

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import leaderboardRouter from "./routes/leaderboard.js";
 import userRouter from "./routes/user.js";
+import gameRouter from "./routes/game.js";
+import websockets from "./gameServer.js";
 
-const port = 80;
 const app = express();
+const port = 80;
+
+const server = app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}\n\n`);
+});
+
+websockets(server);
 
 app.use("/leaderboard", leaderboardRouter);
 app.use("/user", userRouter);
-
-app.listen(port, () => {
-  console.log(`Omok listening on port ${port}`);
-});
+app.use("/game", gameRouter);
